de842d9121b803ca0531c42a3efbcec91661e051
Max-GO-IT <terletskymaxim@ukr.net>
2024-07-20 23:01:14 +0300

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,4 @@
-const Product = () => {
-  return (
-    <div>
-      <h2>Cookies</h2>
-			<p>Price: 999 credits</p>
-    </div>
-  );
-};
+import Product from "./Products";
 
 export default function App() {
   return (
